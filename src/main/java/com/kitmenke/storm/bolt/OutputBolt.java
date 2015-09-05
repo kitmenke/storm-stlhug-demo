585d0ac54769a0e9ed379e75c6295833cb02fad9
Kit Menke <cwmenke@gmail.com>
2015-09-05 18:14:05 -0500

    Inherit from BaseBasicBolt and add error handling.

--- src/main/java/com/kitmenke/storm/bolt/OutputBolt.java
+++ src/main/java/com/kitmenke/storm/bolt/OutputBolt.java
@@ -1,33 +1,29 @@
 package com.kitmenke.storm.bolt;
 
-import java.util.Map;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import backtype.storm.task.OutputCollector;
-import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.BasicOutputCollector;
 import backtype.storm.topology.OutputFieldsDeclarer;
-import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.topology.base.BaseBasicBolt;
 import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
 
-public class OutputBolt extends BaseRichBolt {
+public class OutputBolt extends BaseBasicBolt {
 	private static final long serialVersionUID = 1L;
 	private static Logger LOG = LoggerFactory.getLogger(OutputBolt.class);
-	OutputCollector _collector;	
-	
-	@Override
-	public void prepare(Map stormConf, TopologyContext context,
-			OutputCollector collector) {
-		_collector = collector;
-	}
 
 	@Override
-	public void execute(Tuple input) {
-		String word = input.getString(0);
-		int count = input.getInteger(1);
-		LOG.info("{} = {}", word, count);
-		_collector.ack(input);
+	public void execute(Tuple tuple, BasicOutputCollector collector) {
+		try {
+			String word = tuple.getString(0);
+			int count = tuple.getInteger(1);
+			LOG.info("{} = {}", word, count);
+			collector.emit(new Values(word, count));
+		} catch (Exception e) {
+			LOG.error("OutputBolt error", e);
+			collector.reportError(e);
+		}
 	}
 
 	@Override
